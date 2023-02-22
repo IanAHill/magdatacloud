b7ef2670e3110f3a0e35eef170f234abefd190ec
Frank Wiles <frank@revsys.com>
2023-02-22 10:20:49 -0600

    Cleanup and refactor

--- magdataapp/taxes.py
+++ magdataapp/taxes.py
@@ -10,6 +10,7 @@
 
 import models
 
+
 def extract_taxes(invoice):
 
     ##INDIANA############################################################
@@ -17,25 +18,30 @@ def extract_taxes(invoice):
 
     # IN Vape Tax == 15% of wholesale cost (purchase_price) for Closed Systems (Disposable Vapes and Cloud 8 Vapes)
     if invoice.invoice_level_tax_authority == "IN":
-        for line in invoice.line_items.select_related("item").all():  # select_related tells the ORM to join against the Item
-            if line.item.category_name == "Disposable Vapes":
-                return line.item_total - line.item.purchase_price*line.quantity*.15
-            elif line.category_name == "Cloud 8":
-                if line.item.reporting_sub_category == "1ML Cartridge":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
-                elif line.item.reporting_sub_category == "1ML Disposable":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
-                elif line.item.reporting_sub_category == "1ML Disposables":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
-                elif line.item.reporting_sub_category == "2ML Disposables":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
-                elif line.item.reporting_sub_category == "2ML Pro Disposables":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
-                else:
-                    return line.item_total
-            else:
-                return line.item_total
+        for line in invoice.line_items.select_related(
+            "item"
+        ).all():  # select_related tells the ORM to join against the Item
+            category = line.item.category_name
+            total = line.item_total
+            price = line.item.purchase_price
+            qty = line.quantity
+            tax = 0.15
 
+            if category == "Disposable Vapes":
+                return total - price * qty * tax
+            elif category == "Cloud 8":
+                matching_categories = [
+                    "1ML Cartridge",
+                    "1ML Disposable",
+                    "1ML Disposables",
+                    "2ML Disposables",
+                    "2ML Pro Dispostables",
+                ]
+
+                if line.item.reporting_sub_category in matching_categories:
+                    return total - price * qty * tax
+
+            return total
 
     ##KENTUCKY############################################################
     ######################################################################
@@ -44,20 +50,20 @@ def extract_taxes(invoice):
     elif invoice.invoice_level_tax_authority == "KY":
         for line in invoice.line_items.select_related("item").all():
             if line.item.category_name == "Disposable Vapes":
-                return line.item_total*1.50*line.item.retail_unit_in_wholesale
+                return line.item_total * 1.50 * line.item.retail_unit_in_wholesale
             elif line.item.category_name == "Vape Juice":
-                return line.item_total/1.15
+                return line.item_total / 1.15
             elif line.item.category_name == "Cloud 8":
                 if line.item.reporting_sub_category == "1ML Cartridge":
-                    return line.item_total*1.50*line.item.retail_unit_in_wholesale
+                    return line.item_total * 1.50 * line.item.retail_unit_in_wholesale
                 elif line.item.reporting_sub_category == "1ML Disposable":
-                    return line.item_total*1.50*line.item.retail_unit_in_wholesale
+                    return line.item_total * 1.50 * line.item.retail_unit_in_wholesale
                 elif line.item.reporting_sub_category == "1ML Disposables":
-                    return line.item_total*1.50*line.item.retail_unit_in_wholesale
+                    return line.item_total * 1.50 * line.item.retail_unit_in_wholesale
                 elif line.item.reporting_sub_category == "2ML Disposable":
-                    return line.item_total*1.50*line.item.retail_unit_in_wholesale
+                    return line.item_total * 1.50 * line.item.retail_unit_in_wholesale
                 elif line.item.reporting_sub_category == "2ML Pro Disposable":
-                    return line.item_total*1.50*line.item.retail_unit_in_wholesale
+                    return line.item_total * 1.50 * line.item.retail_unit_in_wholesale
                 else:
                     return line.item_total
             else:
@@ -70,25 +76,34 @@ def extract_taxes(invoice):
     elif invoice.invoice_level_tax_authority == "OH":
         for line in invoice.line_items.all():
             if line.item.category_name == "Disposable Vapes":
-                return line.item_total - line.item.e_liquid_ml*line.quantity*.10
+                return line.item_total - line.item.e_liquid_ml * line.quantity * 0.10
             elif line.item.category_name == "Vape Juice":
-                return line.item_total - line.item.e_liquid_ml*line.quantity*.10
+                return line.item_total - line.item.e_liquid_ml * line.quantity * 0.10
             elif line.item.category_name == "Cloud 8":
                 if line.item.reporting_sub_category == "1ML Cartridge":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.10
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.10
+                    )
                 elif line.item.reporting_sub_category == "1ML Disposable":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.10
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.10
+                    )
                 elif line.item.reporting_sub_category == "1ML Disposables":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.10
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.10
+                    )
                 elif line.item.reporting_sub_category == "2ML Disposable":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.10
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.10
+                    )
                 elif line.item.reporting_sub_category == "2ML Pro Disposable":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.10
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.10
+                    )
                 else:
                     return line.item_total
             else:
                 return line.item_total
-
 
     ##WEST VIRGINIA#######################################################
     ######################################################################
@@ -97,47 +112,71 @@ def extract_taxes(invoice):
     elif invoice.invoice_level_tax_authority == "WV":
         for line in invoice.line_items.all():
             if line.item.category_name == "Disposable Vapes":
-                return line.item_total - line.item.e_liquid_ml*line.quantity*.075
+                return line.item_total - line.item.e_liquid_ml * line.quantity * 0.075
             elif line.item.category_name == "Vape Juice":
-                return line.item_total - line.item.e_liquid_ml*line.quantity*.075
+                return line.item_total - line.item.e_liquid_ml * line.quantity * 0.075
             elif line.item.category_name == "Cloud 8":
                 if line.item.reporting_sub_category == "1ML Cartridge":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.075
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.075
+                    )
                 elif line.item.reporting_sub_category == "1ML Disposable":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.075
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.075
+                    )
                 elif line.item.reporting_sub_category == "1ML Disposables":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.075
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.075
+                    )
                 elif line.item.reporting_sub_category == "2ML Disposable":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.075
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.075
+                    )
                 elif line.item.reporting_sub_category == "2ML Pro Disposable":
-                    return line.item_total - line.item.e_liquid_ml*line.quantity*.075
+                    return (
+                        line.item_total - line.item.e_liquid_ml * line.quantity * 0.075
+                    )
                 else:
                     return line.item_total
             else:
                 return line.item_total
 
-
     ##ILLINOIS############################################################
     ######################################################################
 
-    #IL Vape Tax == 15% wholesale cost for open and closed systems
+    # IL Vape Tax == 15% wholesale cost for open and closed systems
     elif invoice.invoice_level_tax_authority == "IL":
         for line in invoice.line_items.all():
             if line.item.category_name == "Disposable Vapes":
-                return line.item_total - line.item.purchase_price*line.quantity*.15
+                return line.item_total - line.item.purchase_price * line.quantity * 0.15
             elif line.item.category_name == "Vape Juice":
-                return line.item_total - line.item.purchase_price*line.quantity*.15
+                return line.item_total - line.item.purchase_price * line.quantity * 0.15
             elif line.category_name == "Cloud 8":
                 if line.item.reporting_sub_category == "1ML Cartridge":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
+                    return (
+                        line.item_total
+                        - line.item.purchase_price * line.quantity * 0.15
+                    )
                 elif line.item.reporting_sub_category == "1ML Disposable":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
+                    return (
+                        line.item_total
+                        - line.item.purchase_price * line.quantity * 0.15
+                    )
                 elif line.item.reporting_sub_category == "1ML Disposables":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
+                    return (
+                        line.item_total
+                        - line.item.purchase_price * line.quantity * 0.15
+                    )
                 elif line.item.reporting_sub_category == "2ML Disposables":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
+                    return (
+                        line.item_total
+                        - line.item.purchase_price * line.quantity * 0.15
+                    )
                 elif line.item.reporting_sub_category == "2ML Pro Disposables":
-                    return line.item_total - line.item.purchase_price*line.quantity*.15
+                    return (
+                        line.item_total
+                        - line.item.purchase_price * line.quantity * 0.15
+                    )
                 else:
                     return line.item_total
             else:
