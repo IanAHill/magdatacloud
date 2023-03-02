ba5daaa3c2e50444b055ccbe5d0e4bf16bf92cbe
Ian <ian@mag.industries>
2023-03-02 15:02:13 -0500

    updating csv reader

--- magdataapp/management/commands/import_csv.py
+++ magdataapp/management/commands/import_csv.py
@@ -1,4 +1,5 @@
 import djclick as click
+import csv
 
 from magdataapp.models import *
 
@@ -8,6 +9,15 @@ from magdataapp.models import *
 def cli(filename):
     print(filename)
 
+
+
+
+    with open('invoice.csv') as invoice_csv:
+        invoice_data = invoice_csv.readlines()
+    
+    for line in invoice_data:
+        line_item = Invoice_Line_Item.objects.get_or_create()
+        pass
     ####
     # Write csv import code here
     # customer, created = Customer.objects.get_or_create(...)
