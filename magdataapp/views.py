4d6bdf2a0987c4ceed509b3ca71b5175e316b9b7
Ian <ian@ianahill.com>
2023-03-20 15:51:09 -0400

    added utils.py, matplotlib charts for views.py

--- magdataapp/views.py
+++ magdataapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import *
+from .utils import get_plot
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    qs = Invoice_Line_Item.objects.all()
+    x = [x.item.item_name for x in qs]
+    y = [y.item_total for y in qs]
+    chart = get_plot(x, y)
+    return render(request, "home.html", {'chart': chart})
