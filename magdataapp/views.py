94227b6fbec9c12b39e407c0276bb396f3e36f4f
Frank Wiles <frank@revsys.com>
2023-03-22 10:59:40 -0500

    Add chart

--- magdataapp/views.py
+++ magdataapp/views.py
@@ -1,6 +1,13 @@
+import json
 from django.shortcuts import render
 from .models import *
 from .utils import get_plot
+from django.contrib.auth.decorators import login_required
+
+# Auth example
+# @login_required
+# def protectd_view(request):
+#   pass
 
 # Create your views here.
 def home(request):
@@ -8,7 +15,40 @@ def home(request):
     x = [x.item.item_name for x in qs]
     y = [y.item_total for y in qs]
     chart = get_plot(x, y)
-    return render(request, "home.html", {'chart': chart})
+    return render(
+        request,
+        "home.html",
+        {
+            "chart": chart,
+            "chartdatajson": json.dumps([12, 11, 19, 5, 2, 3]),
+        },
+    )
+
 
 def customers(request):
-    return render(request, "customers.html", {})
+    customers = Customer.objects.all()
+    return render(
+        request,
+        "customers.html",
+        {
+            "customers": customers,
+        },
+    )
+
+
+# This would be accessed with
+# /fake-customer/?q=frank
+def fake_customer_search(request):
+    q = request.GET.get("q", None)
+    qs = Customer.objects.all()
+
+    if q is not None:
+        customers = qs.filter(customer_name__icontains=q)
+
+    return render(
+        request,
+        "customers.html",
+        {
+            "customers": customers,
+        },
+    )
