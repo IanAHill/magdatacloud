4ab5ec14364f8f4613ab20a5fea0c33295eb3f3d
Ian <ian@ianahill.com>
2023-03-24 13:43:11 -0400

    added python anywhere to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9p_u!q13o&^ugy%60=*anyofk13mc%a+wne$280bk7yk8(y#_g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ianahill.pythonanywhere.com']
 
 
 # Application definition
